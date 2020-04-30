709930b5da5201582ca989b3e080d640812933e8
wTimml <wtl25@hotmail.com>
2020-04-30 20:36:23 -0300

    animation

---
+++ OneDrive/Documentos/animation css/animation-css/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+
+      <div className="pai">
+          
+          <img className="char" alt="" src="https://66.media.tumblr.com/48cd9cc09400e5499b6309c6bf1e0a97/tumblr_npuxsqaKaC1tbahjdo1_1280.gif"/>
+          <img className="bomb" alt="" src="https://www.nicepng.com/png/full/154-1549731_pixel-bomb-pixel-monster-gif.png"/>
+          
+      </div>
+  );
+}
+
+export default App;
